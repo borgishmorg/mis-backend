590ef4f9c0a39f4ad8044f922b64a39ee26ea3ea
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-04-29 13:01:39 +0200

    feat: add auth module

--- app/api/router.py
+++ app/api/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from .get_status import get_status
+from .auth import router as auth_router
 
 router = APIRouter(
     prefix='/api/v1'
 )
+router.include_router(auth_router)
 router.add_api_route(
     path='/status',
     endpoint=get_status,
