27b3b5afe24dcd5c22806941691cbccc2c28c424
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-05-02 14:20:07 +0200

    feat: add migration for users table

---
+++ alembic/versions/6408529a3a8d_add_user_model.py
@@ -0,0 +1,30 @@
+"""Add user model
+
+Revision ID: 6408529a3a8d
+Revises: 
+Create Date: 2021-05-02 14:13:41.129024
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6408529a3a8d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users',
+    sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+    sa.Column('login', sa.String(length=255), nullable=False),
+    sa.Column('password_hash', sa.String(length=255), nullable=False),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('login')
+    )
+
+
+def downgrade():
+    op.drop_table('users')
