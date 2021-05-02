45829a42ef2661e27c5e8dfcbb2097336d397f83
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2021-05-02 21:51:52 +0200

    refactor: refactor users controller

--- app/api/users/controller.py
+++ app/api/users/controller.py
@@ -1,4 +1,5 @@
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy.exc import IntegrityError
 from app.hash import generate_password_hash
 from app.services import session_scope
 from app.models import (
@@ -15,34 +16,20 @@ class UserException(Exception):
 
 class UsersController:
 
-    def is_user_exists(
-        self,
-        login: str
-    ) -> bool:
-        'Checks is user with current login exists'
-        with session_scope() as session:
-            return (
-                session
-                .query(UserModel)
-                .filter(UserModel.login == login)
-                .count()
-            ) > 0
-
     def create_user(
         self,
         user_login: UserLogin
     ) -> User:
         'Creates new user with specified login and password'
-        if self.is_user_exists(user_login.login):
-            raise UserException(f'User with login "{user_login.login}" alredy exists')
-
         hash = generate_password_hash(user_login.password).hex()
         user = UserModel(
             login=user_login.login, 
             password_hash=hash
         )
-
-        with session_scope() as session:
-            session.add(user)
-            session.flush()
-            return User(**jsonable_encoder(user))
+        try:
+            with session_scope() as session:
+                session.add(user)
+                session.flush()
+                return User(**jsonable_encoder(user))
+        except IntegrityError:
+            raise UserException(f'User with login "{user_login.login}" alredy exists')
